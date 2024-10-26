db8fc1222f48d1c5e43c41830146667585bf033c
rehnuma14 <rehnuma.14.11@gmail.com>
2024-10-27 00:08:27 +0600

    Add files via upload

---
+++ hw.js
@@ -0,0 +1,70 @@
+console.log("Solution of problem 1");
+let a=10,b=5;
+add=a+b,sub=a-b,mul=a*b,div=a/b;
+console.log("Addition="+add);
+console.log("Substraction="+sub);
+console.log("Multiplication="+mul);
+console.log("division="+div);
+
+console.log(" ");
+console.log("Solution of problem 2");
+let celsius=172.5;
+fahrenheit=(celsius*9/5)+32;
+console.log("Fahrenheit="+fahrenheit);
+
+console.log(" ");
+console.log("Solution of problem 3");
+let x=-123;
+if(x>0)
+{
+    console.log("Positive");
+}
+else if(x<0)
+ {
+     console.log("Negative");
+ }
+ else
+    {
+        console.log("Zero");
+    }
+
+
+console.log(" ");
+console.log("Solution of problem 4");
+let n=80;
+if(n>=90 && n<=100)
+{
+    console.log("A");
+}
+else if(n>=80 && n<=89)
+    {
+        console.log("B");
+    }
+ else if(n>=70 && n<789)
+        {
+            console.log("C");
+        }
+        else if(n>=60 && n<=69)
+            {
+                console.log("D");
+            }
+            else if(n>=0 && n<=59)
+                {
+                    console.log("F");
+                }
+                else
+                    {
+                        console.log("INCORRECT");
+                    }
+
+console.log(" ");
+console.log("Solution of problem 5"); 
+let num=50;
+if(num%2==0)
+{
+    console.log("EVEN");
+}
+else
+    {
+        console.log("ODD");
+    }
